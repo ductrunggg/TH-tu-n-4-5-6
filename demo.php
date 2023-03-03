32eb798007497d8d4ebc54058f41995cfbad453b
ductrunggg <118343699+ductrunggg@users.noreply.github.com>
2023-03-03 11:41:26 +0700

    Add files via upload

---
+++ demo.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title> Chào mừng đến với cửa hàng dụng cụ y tế </title>
+</head>
+    <body>
+    <center><h1> CỬA HÀNG DỤNG CỤ Y TẾ </h1></center>
+    </body>
+    <nav>
+        <ul>
+            <li><a href="#link"> Chức năng </a></li>
+            <li><a href="#link"> Mua hàng </a></li>
+            <li><a href="#link"> Giỏ hàng </a></li>
+        </ul>
+    </nav>
+</html>
